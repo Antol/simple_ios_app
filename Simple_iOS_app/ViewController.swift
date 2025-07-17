55d7fb810e6913cf9a73d65d5eeceaa368d9e022
Antol <antol.peshkov@gmail.com>
2025-07-17 14:09:41 +0600

    Initial Commit

---
+++ Simple_iOS_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple_iOS_app
+//
+//  Created by Antol on 17.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
